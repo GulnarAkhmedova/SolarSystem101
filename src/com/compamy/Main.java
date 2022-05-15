db952e87e0c46f5cefe2d0b0694292d15708b9e0
elnar <elnarasabira@gmal.com>
2022-05-15 11:19:31 -0700

    Solar System Basic Ready

---
+++ src/com/compamy/Main.java
@@ -0,0 +1,13 @@
+package com.compamy;
+
+public class Main {
+    public static void main(String[] args) {
+SolarSystem FactorX=new SolarSystem();
+FactorX.Sun="1";
+FactorX.Moon="4";
+FactorX.Planet="2";
+FactorX.Stars="10000";
+
+    }
+}
+
---
+++ src/com/compamy/SolarSystem.java
@@ -0,0 +1,9 @@
+package com.compamy;
+
+public class SolarSystem {
+    String Sun;
+    String Moon;
+    String Planet;
+    String Stars;
+
+}
